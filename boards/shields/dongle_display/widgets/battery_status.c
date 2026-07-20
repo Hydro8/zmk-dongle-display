c4dd98bd1ee1c9fc6c0f7541d98f57280713ce34
Hydro <erwannlaville@gmail.com>
2026-07-20 23:35:18 +0200

    Change police

--- boards/shields/dongle_display/widgets/battery_status.c
+++ boards/shields/dongle_display/widgets/battery_status.c
@@ -153,10 +153,12 @@ int zmk_widget_dongle_battery_status_init(struct zmk_widget_dongle_battery_statu
 
         lv_canvas_set_buffer(image_canvas, battery_image_buffer[i], 5, 8, LV_IMG_CF_TRUE_COLOR);
 
-        lv_obj_align(image_canvas, LV_ALIGN_TOP_RIGHT, -(i * 35), 0);
-        lv_obj_align(battery_label, LV_ALIGN_TOP_RIGHT, -(i * 35) - 7, 0);
-
+        // Cacher l'icône pour toujours
         lv_obj_add_flag(image_canvas, LV_OBJ_FLAG_HIDDEN);
+
+        // Aligner le texte, décalé de 35px vers la droite à chaque fois
+        lv_obj_align(battery_label, LV_ALIGN_TOP_LEFT, i * 35, 0);
+
         lv_obj_add_flag(battery_label, LV_OBJ_FLAG_HIDDEN);
         
         battery_objects[i] = (struct battery_object){
